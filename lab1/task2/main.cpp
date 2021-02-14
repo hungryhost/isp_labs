e314e00a72d5c5517432aea2cd411d201a122441
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-02-14 18:19:59 +0300

    added sorting, zero counter and replace

--- lab1/task2/main.cpp
+++ lab1/task2/main.cpp
@@ -1,81 +1,98 @@
 #include <iostream>
 #include <vector>
 #include <bits/stdc++.h>
+#include <algorithm>
+#include <chrono>
+class Task2 {
 
-std::vector<int> init_and_fill_vector(int size) {
-   // initialize and fill the vector for the task 1
-   std::vector<int> vect(size, 0);
-   std::vector<int>::iterator i;
-   for(i=vect.begin();i!=vect.end();i+=1){
-        *i = rand() % 1000;
-   }
-   for(i=vect.begin();i!=vect.end();i+=2){
-        *i = rand() % 1000 - 1500;
-    }
-   std::cout << "The elements of vector are:\n";
-   // Print the values in the vector
-   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-   std::default_random_engine e(seed);
-   std::shuffle(vect.begin(), vect.end(), e);
-   for(i=vect.begin();i!=vect.end();i++)
-        std::cout<<*i<<" ";
+    public:
 
-   return vect;
-}
-
-std::vector<int> find_first_third_positive(std::vector<int> initial_vector){
-    // result 2d vector with ndexes of first three posititve
-    // numbers. The last one is the one we're looking for.
-    std::vector<int> result_vector;
-    std::vector<int>::iterator i = initial_vector.begin();
-    // counter for the positive numbers
-    int count = 0;
-
-    while(i != initial_vector.end() && count < 3) {
-        if (*i > 0) {
-            result_vector.push_back(i-initial_vector.begin());
-            ++i;
-            count++;
+    int count_zeroes_in_matrix(std::vector<std::vector<int>> &mat){
+        int z_count = 0;
+        std::vector<std::vector<int>>::iterator row;
+        for (row = mat.begin(); row != mat.end(); row++) {
+            z_count = z_count + count(row->begin(), row->end(), 0);
         }
-        else ++i;
+        return z_count;
+
     }
-    return result_vector;
+    void multiply_rows_by_number_of_zeroes(std::vector<std::vector<int>> &mat){
+        int N = mat.size();
+        int count = 0;
+        int new_val;
+        int zeroes = count_zeroes_in_matrix(mat);
+        std::vector<std::vector<int> >::iterator row;
+        std::vector<int>::iterator col;
+        for (row = mat.begin(); row != mat.end(); row++) {
+            for (col = row->begin(); col != row->end(); col++) {
+                if (*col < 0){
+                    new_val = *col/zeroes;
+                    replace(row->begin(), row->end(), *col, new_val);
+                }
+
+            }
+        }
+    }
+
+    void solve_sort(int sj, std::vector<std::vector<int>> &mat){
+       // method for sorting
+        int n = mat.size();
+        int k = sj;
+        std::vector<int> temp;
+        int l;
+        for(int j = k+1; j < n; j++){
+            temp.push_back(mat[k][j]);
+
+        }
+        sort(temp.begin(), temp.end(),  std::greater<int>());
+        l = 0;
+        for(int j = k+1; j < n; j++){
+            mat[k][j] = temp[l];
+            //temp.push_back(mat[k][j]);
+            l++;
+        }
+
+    }
 
 
-}
+    std::vector<std::vector<int>> diagonalSort(std::vector<std::vector<int>>& mat) {
+          int n = mat.size();
 
-int get_third_element_index(std::vector<int> input_vector){
-    std::vector<int> result = find_first_third_positive(input_vector);
-    if (result.size() < 3){
-        return -1;
-    }
-    return result.back();
-}
+          for(int j = 0; j < n; j++){
+            solve_sort(j, mat);
+          }
+          return mat;
+       }
 
-void task1(bool bad){
-    std::vector<int> myvect;
-    if (bad == true) {
-        myvect = {-1, -1, -333, -49, -1, -4, -129, -121, -11, -12,
-                 -11, -11, -333, -491, -11, -4, -129, -121, -11, -12,
-                 -21, -11, -3313, -439, -112, -4, -129, -121, -11, -12,
-                 -1, -1, -3333, -149, -18, -48, -1299, -1421, -121, -11};
-    }
-    else {
-        myvect = init_and_fill_vector(40);
-    }
-    int result = get_third_element_index(myvect);
-    if (result == -1) {
-        std::cout << '\n' << "Result of task 1: Array does not have 3 positive numbers"
-                  << std::endl;
-    }
-    else{
-        std::cout << '\n' << "Result of task 1: " << result << std::endl;
-    }
-}
+    void printElement(std::vector<std::vector<int>>& arr)
+       {
+
+           // Traverse the 2D vector
+           for (int i = 0; i < arr.size(); i++) {
+               for (int j = 0; j < arr[0].size(); j++) {
+                   std::cout << arr[i][j] << ' ';
+               }
+               std::cout << std::endl;
+           }
+       }
+};
+
 int main()
 {
-    task1(false);
-    task1(true);
+    std::vector<std::vector<int>> v = {{3,-32,22,21},{2,10,0,9},{-28,2,-8,0},{4,5,-31,-8}};
+    Task2 task2;
+    task2.printElement(v);
+     std::cout << std::endl;
+    std::vector<std::vector<int>> res = task2.diagonalSort(v);
+     std::cout << std::endl;
+    task2.printElement(res);
+     std::cout << std::endl;
+    int i = task2.count_zeroes_in_matrix(res);
+
+
+    std::cout << "Number of zeroes: " << i << ' ' << std::endl;
+    task2.multiply_rows_by_number_of_zeroes(res);
+    task2.printElement(res);
     return 0;
 }
 
